501abd5bc03032e342cca596adee815c41118011
ellniemm <adelweis876@gmail.com>
2025-01-21 10:11:58 +0700

    dashboard

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -4,5 +4,11 @@
 @section('title', 'Dashboard')
 
 @section('main')
-    
+
+    welcome, momo!
+    <div class="mt-4 p-4">
+        <iframe src="https://sinergi.exirostudio.my.id" width="800" height="600"></iframe>
+    </div>
+
+
 @endsection
